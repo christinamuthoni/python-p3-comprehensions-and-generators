adf873f995f38b87eb632db6dd5e66ade512b511
christine <christine.muriungi@student.moringaschool.com>
2023-12-08 23:47:12 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    return even_numbers
+            
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+result = return_evens(num_list)
+print(result)
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence_list = [sentence_list + "!" for sentence_list in sentence_list]
+    return new_sentence_list
+
+sentence_list = ("am home")    
+result = make_exclamation(sentence_list)
+print(result)
